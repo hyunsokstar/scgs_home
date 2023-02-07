ba04e39dc1bcc9aa7cd1b9bf224fbc4b47dc2bab
hyunsok <hyunsokstar@gmail.com>
2023-02-07 18:01:07 +0900

    update for 0207 1800

--- front/src/routes/UploadRoom.tsx
+++ front/src/routes/UploadRoom.tsx
@@ -1,12 +1,25 @@
-import { Box } from "@chakra-ui/react";
-import useHostOnlyPage from "../components/HostOnlyPage";
-import ProtectedPage from "../components/ProtectedPage";
+import { Box, Input } from "@chakra-ui/react";
+// import useHostOnlyPage from "../components/HostOnlyPage";
+// import ProtectedPage from "../components/ProtectedPage";
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+} from '@chakra-ui/react'
+
 
 export default function UploadRoom() {
     // useHostOnlyPage();
     return (
-        <Box>
-            <h1>upload roommmmmmm</h1>
+        <Box ml={200} mr={200}>
+
+            <FormControl>
+                <FormLabel>Email address</FormLabel>
+                <Input type='email' />
+                <FormHelperText>We'll never share your email.</FormHelperText>
+            </FormControl>
+
         </Box>
     );
 }
